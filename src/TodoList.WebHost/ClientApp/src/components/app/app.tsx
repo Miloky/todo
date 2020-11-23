28e8696230c05fdfd187412f02d4aed0a5a73c5d
Miloky <monmorti@gmail.com>
2020-11-23 23:22:28 +0200

    migrate to scss modules

--- src/TodoList.WebHost/ClientApp/src/components/app/app.tsx
+++ src/TodoList.WebHost/ClientApp/src/components/app/app.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent, ReactElement } from 'react';
-import classes from  './app.module.css';
+import classes from  './app.module.scss';
 
 const App: FunctionComponent = (): ReactElement => {
   return <div className={classes.red}>Test</div>;
